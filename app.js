b25f25e429f008ea213b1ffb4944db534d7d3de0
Pedro <lopeznovais@hotmail.com>
2020-04-25 06:43:25 -0400

    Correcoes

--- app.js
+++ app.js
@@ -46,7 +46,7 @@
         app.set("view engine", "handlebars")
     // Mongoose
         mongoose.Promise = global.Promise
-        mongoose.connect(db.mongoURI).then(() =>{
+        mongoose.connect(db.mongoURI, { useNewUrlParser: true }).then(() =>{
             console.log("Conectado ao mongoDB")
         }).catch((err) => {
             console.log("Erro ao se conectar ao mongoDB " + err)
